0c1ffbb6facca414aefab901f9ac6672cb5ce9ff
Nurul Hidayati Khusnia <95520277+Conannia@users.noreply.github.com>
2022-04-30 09:50:02 +0700

    table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import React, { useEffect } from 'react';
+
 
 function App() {
+
+
+  useEffect(() => {
+    Axios.post(`https://kipi.covid19.go.id/api/get-province`)
+      .then(hasil => {
+        console.log(hasil.data)
+      })
+
+  });
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='table'>
+        <table>
+          <thead>
+            <th>NO</th>
+            <th>NAMA FASKES</th>
+            <th>ALAMAT</th>
+            <th>KATEGORI</th>
+            <th>TOTAL VAKSIN 1:</th>
+            <th>TOTAL VAKSIN 2:</th>
+            <th>TOTAL VAKSIN 1 DAN 2:</th>
+          </thead>
+          <tbody>
+            <td>01</td>
+            <td>Type Text</td>
+            <td>Type Text</td>
+            <td>Type Text</td>
+            <td>Type Number</td>
+            <td>Type Number</td>
+            <td>Type Number</td>
+          </tbody>
+        </table>
+      </div>
+
+
     </div>
   );
 }
